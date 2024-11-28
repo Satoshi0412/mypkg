28a7e90faf6e8478f21159f3e0bf9f82a435cca9
Satoshi Ono <s23C1035NN@s.chibakoudai.jp>
2024-11-28 15:16:55 +0900

    test.bash

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = mypkg.talker:main'
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
         ],
     },
 )
